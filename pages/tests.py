22dbde2440be5e8d87d2dbe20a7cc27d4a3dd185
moayadalhaj <moayad.alhaj21@gmail.com>
2021-11-17 23:07:48 +0200

    added custom user model and use tailwind for styling

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.urls import reverse
 
 # Create your tests here.
+
+class test_for_all(TestCase):
+    def test_home_page_status_code(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_tepmlate(self):
+        url = reverse('home')
+        response = self.client.get(url)
+        self.assertTemplateUsed(response, 'pages/home.html')
